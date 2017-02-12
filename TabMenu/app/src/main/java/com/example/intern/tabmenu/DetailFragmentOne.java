4d518c3b839e33e7f82c8ba67b1cb4037245e7e6
ThienCan <minhthientran2501@gmail.com>
2017-02-12 19:09:58 +0700

    Fix toolbar back button

--- TabMenu/app/src/main/java/com/example/intern/tabmenu/DetailFragmentOne.java
+++ TabMenu/app/src/main/java/com/example/intern/tabmenu/DetailFragmentOne.java
@@ -1,8 +1,9 @@
 package com.example.intern.tabmenu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class DetailFragmentOne extends AppCompatActivity {
@@ -17,9 +18,22 @@ public class DetailFragmentOne extends AppCompatActivity {
 
         try {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
         }catch(NullPointerException ex){
             Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
 
         }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
